c3ac0646da9025e9a66d24be16268a97d136e924
donhwi94 <ehsgnl123@naver.com>
2022-09-18 02:16:35 +0900

    first commit

---
+++ src/main/java/com/teamtopping/toppingup/ToppingupApplication.java
@@ -0,0 +1,13 @@
+package com.teamtopping.toppingup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToppingupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToppingupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamtopping/toppingup/ToppingupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamtopping.toppingup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToppingupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
